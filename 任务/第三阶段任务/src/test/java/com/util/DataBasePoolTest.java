a52e3b957d569cc9d43fa7ab2de2be90467498aa
黄诃华 <3116725220@qq.com>
2023-02-16 14:54:56 +0800

    test(第三周任务/java): 双线程测试

--- 任务/第三阶段任务/src/test/java/com/util/DataBasePoolTest.java
+++ 任务/第三阶段任务/src/test/java/com/util/DataBasePoolTest.java
@@ -4,24 +4,53 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 
 public class DataBasePoolTest {
     @Test
-    void TestGetConnection() throws InterruptedException, SQLException {
+    void TestGetConnection() throws InterruptedException {
+        Logger logger = MyLogger.getLogger();
 
-        DateBasePool dateBasePool = new DateBasePool();
-
-        for (int i = 0; i < 50; i++) {
-            Connection connection = dateBasePool.getConnection();
-            System.out.println("第" + i + "个 " + connection);
-            if (i % 2 == 0 && connection != null) {
-                connection.close();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 30; i++) {
+                    Connection connection = DateBasePool.getConnection();
+                    logger.info("T1 线程获取第" + i + "个 " + connection);
+                    if (i <= 10 && connection != null) {
+                        logger.info("T1 释放第" + i + "个 " + connection);
+                        try {
+                            connection.close();
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
             }
-        }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 30; i++) {
 
-
-
+                    Connection connection = DateBasePool.getConnection();
+                    logger.info("T2 获取第" + i + "个 " + connection);
+                    if (i <= 10 && connection != null) {
+                        logger.info("T2 释放第" + i + "个 " + connection);
+                        try {
+                            connection.close();
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        Thread.sleep(5000);
 
     }
+
 }
